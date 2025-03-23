cbc3debe2336d6cb66605977b5a3ca2782e9591b
Liam Dvorscak <liam.dvorscak1@gmail.com>
2025-03-23 14:09:06 -0400

    XP system fixes

--- crates/tgar_bot_http/src/routes/xp.rs
+++ crates/tgar_bot_http/src/routes/xp.rs
@@ -2,30 +2,58 @@ use actix_web::{
     HttpResponse, patch,
     web::{Data, Json},
 };
+use futures::future::try_join_all;
 use mongodb::Client;
 
 use crate::models::player::ExpOperation;
 
 #[patch("/set_xp")]
-pub async fn set_xp(client: Data<Client>, operation: Json<ExpOperation>) -> HttpResponse {
-    match tgar_bot_mongodb::set_xp(&client, operation.id, operation.xp).await {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(e) => HttpResponse::InternalServerError().body(e),
+pub async fn set_xp(client: Data<Client>, batch: Json<Vec<ExpOperation>>) -> HttpResponse {
+    let futures: Vec<_> = batch
+        .into_inner()
+        .into_iter()
+        .map(|op| tgar_bot_mongodb::set_xp(&client, op.id, op.xp))
+        .collect();
+
+    let res = try_join_all(futures).await;
+
+    if res.is_err() {
+        HttpResponse::InternalServerError().finish()
+    } else {
+        HttpResponse::Ok().finish()
     }
 }
 
 #[patch("/add_xp")]
-pub async fn add_xp(client: Data<Client>, operation: Json<ExpOperation>) -> HttpResponse {
-    match tgar_bot_mongodb::add_xp(&client, operation.id, operation.xp).await {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(e) => HttpResponse::InternalServerError().body(e),
+pub async fn add_xp(client: Data<Client>, batch: Json<Vec<ExpOperation>>) -> HttpResponse {
+    let futures: Vec<_> = batch
+        .into_inner()
+        .into_iter()
+        .map(|op| tgar_bot_mongodb::add_xp(&client, op.id, op.xp))
+        .collect();
+
+    let res = try_join_all(futures).await;
+
+    if res.is_err() {
+        HttpResponse::InternalServerError().finish()
+    } else {
+        HttpResponse::Ok().finish()
     }
 }
 
 #[patch("/remove_xp")]
-pub async fn remove_xp(client: Data<Client>, operation: Json<ExpOperation>) -> HttpResponse {
-    match tgar_bot_mongodb::remove_xp(&client, operation.id, operation.xp).await {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(e) => HttpResponse::InternalServerError().body(e),
+pub async fn remove_xp(client: Data<Client>, batch: Json<Vec<ExpOperation>>) -> HttpResponse {
+    let futures: Vec<_> = batch
+        .into_inner()
+        .into_iter()
+        .map(|op| tgar_bot_mongodb::remove_xp(&client, op.id, op.xp))
+        .collect();
+
+    let res = try_join_all(futures).await;
+
+    if res.is_err() {
+        HttpResponse::InternalServerError().finish()
+    } else {
+        HttpResponse::Ok().finish()
     }
 }
--- crates/tgar_bot_mongodb/src/lib.rs
+++ crates/tgar_bot_mongodb/src/lib.rs
@@ -5,11 +5,11 @@ use crate::{auto_ranking::try_rank, models::player::Player};
 mod auto_ranking;
 pub mod models;
 
-async fn update_xp(client: &Client, user_id: i64, xp: i64, kind: &str) -> Result<(), String> {
+pub async fn set_xp(client: &Client, user_id: i64, xp: i64) -> Result<(), String> {
     let collection = client.database("game").collection::<Player>("players");
 
     let filter = doc! { "id": user_id };
-    let update = doc! { kind: { "xp": xp } };
+    let update = doc! { "$set": { "xp": xp } };
 
     match collection.update_one(filter, update).await {
         Ok(res) => {
@@ -24,14 +24,40 @@ async fn update_xp(client: &Client, user_id: i64, xp: i64, kind: &str) -> Result
     }
 }
 
-pub async fn set_xp(client: &Client, user_id: i64, xp: i64) -> Result<(), String> {
-    update_xp(client, user_id, xp, "$set").await
-}
-
 pub async fn add_xp(client: &Client, user_id: i64, xp: i64) -> Result<(), String> {
-    update_xp(client, user_id, xp, "$inc").await
+    let collection = client.database("game").collection::<Player>("players");
+
+    let filter = doc! { "id": user_id };
+    let update = doc! { "$inc": { "xp": xp } };
+
+    match collection.update_one(filter, update).await {
+        Ok(res) => {
+            if res.matched_count == 1 {
+                try_rank(user_id as u64).await;
+                Ok(())
+            } else {
+                Err("player not found".to_string())
+            }
+        }
+        Err(e) => Err(e.to_string()),
+    }
 }
 
 pub async fn remove_xp(client: &Client, user_id: i64, xp: i64) -> Result<(), String> {
-    update_xp(client, user_id, -xp, "$inc").await
+    let collection = client.database("game").collection::<Player>("players");
+
+    let filter = doc! { "id": user_id };
+    let update = doc! { "$inc": { "xp": -xp } };
+
+    match collection.update_one(filter, update).await {
+        Ok(res) => {
+            if res.matched_count == 1 {
+                try_rank(user_id as u64).await;
+                Ok(())
+            } else {
+                Err("player not found".to_string())
+            }
+        }
+        Err(e) => Err(e.to_string()),
+    }
 }
