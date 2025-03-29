4d5a184fbbc022911c8c9a56654c5f839973c60a
Liam Dvorscak <liam.dvorscak1@gmail.com>
2025-03-28 23:24:12 -0400

    Fix users type and add xp view command

--- crates/tgar_bot/src/commands.rs
+++ crates/tgar_bot/src/commands.rs
@@ -67,62 +67,82 @@ async fn collect_user_ids(usernames: &Vec<String>) -> Vec<u64> {
 
 pub mod xp {
     use poise::command;
+    use tgar_bot_mongodb::models::player::Player;
     use tgar_bot_utils::mongo_client;
 
     use crate::commands::{Context, Error, check_guild_permissions, collect_user_ids};
 
     /// Parent command for everything xp related
-    #[command(
-        slash_command,
-        guild_only,
-        subcommands("add", "remove", "set"),
-        check = "check_guild_permissions"
-    )]
+    #[command(slash_command, guild_only, subcommands("add", "remove", "set", "view"))]
     pub async fn xp(ctx: Context<'_>) -> Result<(), Error> {
         // impl note: no-op
         Ok(())
     }
 
     /// Adds XP to the specified users
-    #[command(slash_command)]
+    #[command(slash_command, check = "check_guild_permissions")]
     pub async fn add(
         ctx: Context<'_>,
-        #[description = "The usernames of the people you wish to add xp to"] users: Vec<String>,
+        #[description = "The usernames of the people you wish to add xp to"] users: String,
         #[description = "The amount of XP you want to add"] amount: u64,
     ) -> Result<(), Error> {
-        for id in collect_user_ids(&users).await {
+        let user_names: Vec<String> = users.split(',')
+            .map(|s| s.to_string())
+            .collect();
+        for id in collect_user_ids(&user_names).await {
             tgar_bot_mongodb::add_xp(mongo_client().await, id as i64, amount as i64).await?;
         }
-        ctx.reply(format!("Successfully added {} XP to {} user(s)!", amount, users.len())).await?;
+        ctx.reply(format!("Successfully added {} XP to {} user(s)!", amount, user_names.len())).await?;
         Ok(())
     }
 
     /// Removes XP from the specified users
-    #[command(slash_command)]
+    #[command(slash_command, check = "check_guild_permissions")]
     pub async fn remove(
         ctx: Context<'_>,
-        #[description = "The usernames of the people you wish to take xp from"] users: Vec<String>,
+        #[description = "The usernames of the people you wish to take xp from"] users: String,
         #[description = "The amount of XP you want to remove"] amount: u64,
     ) -> Result<(), Error> {
-        for id in collect_user_ids(&users).await {
+        let user_names: Vec<String> = users.split(',')
+            .map(|s| s.to_string())
+            .collect();
+        for id in collect_user_ids(&user_names).await {
             tgar_bot_mongodb::remove_xp(mongo_client().await, id as i64, amount as i64).await?;
         }
-        ctx.reply(format!("Successfully removed {} XP from {} user(s)!", amount, users.len()))
+        ctx.reply(format!("Successfully removed {} XP from {} user(s)!", amount, user_names.len()))
             .await?;
         Ok(())
     }
 
     /// Sets the XP of the specified users
-    #[command(slash_command)]
+    #[command(slash_command, check = "check_guild_permissions")]
     pub async fn set(
         ctx: Context<'_>,
-        #[description = "The usernames of the people you wish to set the XP of"] users: Vec<String>,
+        #[description = "The usernames of the people you wish to set the XP of"] users: String,
         #[description = "The amount of XP you want them to have"] amount: u64,
     ) -> Result<(), Error> {
-        for id in collect_user_ids(&users).await {
+        let user_names: Vec<String> = users.split(',')
+            .map(|s| s.to_string())
+            .collect();
+        for id in collect_user_ids(&user_names).await {
             tgar_bot_mongodb::set_xp(mongo_client().await, id as i64, amount as i64).await?;
         }
-        ctx.reply(format!("Successfully set {} users XP to {}!", users.len(), amount)).await?;
+        ctx.reply(format!("Successfully set {} users XP to {}!", user_names.len(), amount)).await?;
+        Ok(())
+    }
+
+    /// Views the XP that a player has
+    #[command(slash_command)]
+    pub async fn view(
+        ctx: Context<'_>,
+        #[description = "The username of the person you want to view the XP of"] user: String,
+    ) -> Result<(), Error> {
+        let data = Player::from_user_id(
+            *collect_user_ids(&vec![user.clone()]).await.first().expect("couldn't find player")
+                as i64,
+        )
+        .await;
+        ctx.reply(format!("{} has {} XP", user, data.xp)).await.expect("discord api is down");
         Ok(())
     }
 }
